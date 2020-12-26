5fd4a9717ee157f691213e2fa95092aaef3b910e
Benjamin Lewandowski <benjaminlewandowskide@gmail.com>
2020-12-26 15:39:35 +0100

    .

--- src/resources/config/dev/postcss.config.js
+++ src/resources/config/dev/postcss.config.js
@@ -1,15 +1,7 @@
 const presetenv = require("postcss-preset-env");
 const short = require("postcss-short");
-const sass = require("@csstools/postcss-sass");
 
 module.exports = {
-    syntax: "postcss-scss",
     extends: ["stylelint-config-prettier", "stylelint-config-rational-order"],
-    plugins: [
-        sass({
-            includePaths: ["./src/resources/sass/**", "./node_modules"],
-        }),
-        presetenv,
-        short,
-    ],
+    plugins: [presetenv, short],
 };
